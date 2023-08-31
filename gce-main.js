ee8fedd19ca6178a8bd1ab44f66962ecf3c79bcb
Katya Villano <katyavillano@Katyas-MacBook-Pro.local>
2023-08-30 21:09:10 -0400

    update

--- gce-main.js
+++ gce-main.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-if (( document.documentElement.textContent || document.documentElement.innerText
-        ).indexOf('fish') > -1
-      ) {
-        //pop up our video
-      }
+//document.addEventListener('DOMContentLoaded', () => {
+// if (( document.documentElement.textContent || document.documentElement.innerText
+//         ).indexOf('Monday' || 'monday' || 'and') > -1
+//       ) {
+//         //pop up our video
+//        const body = document.querySelector('body');
+//        body.style.backgroundColor = 'blue';
+//       }
+      console.log('here')
+
+      const ourBody = document.querySelector('body');
+      ourBody.style.backgroundColor = 'blue';
+      
 
 
+//})
 
-})
-
-hi
-bye 
