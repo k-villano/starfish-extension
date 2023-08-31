849c6249db8ba789ece91b3c532da8cfff02d03e
Katya Villano <katyavillano@Katyas-MacBook-Pro.local>
2023-08-30 20:17:43 -0400

    update

--- gce-main.js
+++ gce-main.js
@@ -1,1 +1,12 @@
-hello
+document.addEventListener('DOMContentLoaded', () => {
+if (( document.documentElement.textContent || document.documentElement.innerText
+        ).indexOf('fish') > -1
+      ) {
+        //pop up our video
+      }
+
+
+
+})
+
+hi
