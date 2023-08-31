fd7b52a53617555b23035ddf0f122fc36f8d2cd1
Katya Villano <katyavillano@Katyas-MacBook-Pro.local>
2023-08-31 12:55:45 -0400

    update

--- gce-main.js
+++ gce-main.js
@@ -1,16 +1,30 @@
 //document.addEventListener('DOMContentLoaded', () => {
-// if (( document.documentElement.textContent || document.documentElement.innerText
-//         ).indexOf('Monday' || 'monday' || 'and') > -1
-//       ) {
-//         //pop up our video
-//        const body = document.querySelector('body');
-//        body.style.backgroundColor = 'blue';
-//       }
-      console.log('here')
+if (( document.documentElement.textContent || document.documentElement.innerText
+        ).indexOf('About' || 'monday' || 'and' || 'Ocean' ||'ocean') > -1
+      ) {
+        //pop up our video
+        //ourBody.style.backgroundColor = 'blue';
+        const ourBody = document.querySelector('body');
+        ourBody.style.backgroundColor = 'teal'
+        
 
-      const ourBody = document.querySelector('body');
-      ourBody.style.backgroundColor = 'blue';
+        const vidtag = document.createElement('embed')
+        ourBody.appendChild(vidtag)
+        vidtag.setAttribute('src', 'https://www.youtube.com/embed/DHUnz4dyb54?si=pubQ2MSfNxZvLp9C&autoplay=1&mute=1')
+        vidtag.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; allowfullscreen; web-share')
+        vidtag.setAttribute('type', 'video/mp4')
+        vidtag.setAttribute('wmode', 'transparent')
+        vidtag.style.width = '100vw'
+        vidtag.style.height = '100vw'
+        vidtag.style.objectFit = 'cover'
+        vidtag.style.position = 'fixed'
+        vidtag.style.zIndex =  '-1'
+        vidtag.autoplay = true
+        vidtag.setAttribute('frameboard', '0')
+
       
+      }
+      console.log('here')
 
 
 //})
